1f910493d73dcadc94d7b84f32baad4a29d908e4
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-10-07 22:27:33 -0300

    feat: jpql queries, SaleService, SaleMinProjection

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,15 @@
 package com.devsuperior.dsmeta.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -16,7 +17,7 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
 		SaleMinDTO dto = service.findById(id);
@@ -30,8 +31,15 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleMinDTO>> getSummary(
+			@RequestParam(name = "minDate") String minDate,
+			@RequestParam(name = "maxDate") String maxDate,
+			@RequestParam(name = "name") String name,
+			Pageable pageable) {
+
+		Page<SaleMinDTO> dto = service.searchPagedSalesWithInitialDateFinalDateAnSellerPartialName(
+				minDate, maxDate, name, pageable);
+
+		return ResponseEntity.ok(dto);
 	}
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,23 +1,44 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleMinProjection;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
 @Service
 public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleMinDTO> searchPagedSalesWithInitialDateFinalDateAnSellerPartialName(
+			String minDateStr, String maxDateStr, String name, Pageable pageable) {
+
+		LocalDate minDate;
+		LocalDate maxDate;
+
+		try {
+			minDate = LocalDate.parse(minDateStr);
+			maxDate = LocalDate.parse(maxDateStr);
+		} catch (DateTimeParseException e) {
+			throw new IllegalArgumentException("Invalid date format");
+		}
+		Page<SaleMinProjection> saleMinProjectionPage = repository
+				.searchByInitialDateFinalDateAndSellerPartialName(minDate, maxDate, name, pageable);
+        return saleMinProjectionPage.map(SaleMinDTO::new);
 	}
 }
